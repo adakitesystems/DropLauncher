6b6ee7b06fb0d6835d0bca5454cb8e9ee5252351
Adakite Systems <adakitesystems@gmail.com>
2017-01-06 16:27:37 -0600

    Initial commit of new branch

---
+++ src/droplauncher/DropLauncher.java
@@ -0,0 +1,15 @@
+package droplauncher;
+
+/**
+ * @author Adakite Systems
+ * @author adakitesystems@gmail.com
+ */
+public class DropLauncher {
+
+  public static final String PROGRAM_NAME = "DropLauncher";
+  public static final String PROGRAM_VERSION = "0.02a";
+  public static final String PROGRAM_AUTHOR = "Adakite";
+
+  private DropLauncher() {}
+
+}
