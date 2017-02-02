2b5a1c8a33c1590d230b9df26448e39b246538ae
Adakite Systems <adakitesystems@gmail.com>
2017-02-02 15:59:05 -0600

    Add file BotFile.java

---
+++ src/droplauncher/bwheadless/BotFile.java
@@ -0,0 +1,55 @@
+package droplauncher.bwheadless;
+
+import adakite.utils.AdakiteUtils;
+import java.nio.file.Paths;
+
+/**
+ * Container class for bot DLL or EXE files.
+ */
+public class BotFile {
+
+  public enum Type {
+    DLL,
+    CLIENT,
+    UNKNOWN
+  }
+
+  private Type type;
+  private String path;
+
+  public BotFile() {
+    init();
+    this.path = "";
+  }
+
+  public BotFile(String path) {
+    init();
+    setPath(path);
+  }
+
+  private void init() {
+    this.type = Type.UNKNOWN;
+    this.path = "";
+  }
+
+  public Type getType() {
+    return this.type;
+  }
+
+  public String getPath() {
+    return this.path;
+  }
+
+  public void setPath(String path) {
+    this.path = path;
+    String ext = AdakiteUtils.getFileExtension(Paths.get(this.path)).toLowerCase();
+    if (ext.equals("dll")) {
+      this.type = Type.DLL;
+    } else if (ext.equals("exe") || ext.equals("jar")) {
+      this.type = Type.CLIENT;
+    } else {
+      this.type = Type.UNKNOWN;
+    }
+  }
+
+}
