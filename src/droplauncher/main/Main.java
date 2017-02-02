81963146da0c850c45ef69bb489ec02d79b3d9b2
Adakite Systems <adakitesystems@gmail.com>
2017-02-02 01:44:55 -0600

    Test code

--- src/droplauncher/main/Main.java
+++ src/droplauncher/main/Main.java
@@ -1,11 +1,55 @@
 package droplauncher.main;
 
-import droplauncher.mvc.MVC;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application implements EventHandler<ActionEvent> {
+
+  private Stage stage;
+  private Scene scene;
+
+  private Button button;
+  private Button button2;
 
   public static void main(String[] args) {
-    MVC mvc = new MVC();
+//    MVC mvc = new MVC();
+
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) {
+    this.stage = primaryStage;
+
+    this.button = new Button();
+    this.button.setText("Button text here");
+    this.button.setOnAction(this);
+
+    this.button2 = new Button();
+    this.button2.setText("Button text here again");
+    this.button2.setOnAction(this);
+
+    VBox layout = new VBox(20);
+    layout.getChildren().addAll(this.button, this.button2);
+    scene = new Scene(layout, 500, 300);
+
+    this.stage.setTitle("This is my title");
+    this.stage.setScene(scene);
+    this.stage.show();
+  }
+
+  @Override
+  public void handle(ActionEvent event) {
+    if (event.getSource() == this.button) {
+      System.out.println("button1 clicked");
+    } else if (event.getSource() == this.button2) {
+      System.out.println("button2 clicked");
+    }
   }
 
 }
