6a02bba7bf4c40add6c87a2bfb36dba373449e55
Adakite Systems <adakitesystems@gmail.com>
2017-02-11 07:41:40 -0600

    Disable View

--- src/droplauncher/main/Main.java
+++ src/droplauncher/main/Main.java
@@ -1,30 +1,31 @@
 package droplauncher.main;
 
 import droplauncher.mvc.MVC;
-import javafx.application.Application;
-import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Main extends Application {
+//public class Main extends Application {
+public class Main {
 
   private static final Logger LOGGER = LogManager.getLogger();
 
   public static void main(String[] args) {
-    LOGGER.info("start of main");
-    /* Try-catch for sending all exceptions to log4j2. */
-    try {
-      launch(args);
-    } catch (Exception ex) {
-      LOGGER.error("droplauncher", ex);
-    }
-    LOGGER.info("end of main");
+//    LOGGER.info("start of main");
+//    /* Try-catch for sending all exceptions to log4j2. */
+//    try {
+//      launch(args);
+//    } catch (Exception ex) {
+//      LOGGER.error("droplauncher", ex);
+//    }
+//    LOGGER.info("end of main");
+
+    MVC mvc = new MVC();
   }
 
-  @Override
-  public void start(Stage primaryStage) {
-    MVC mvc = new MVC();
-    mvc.start(primaryStage);
-  }
+//  @Override
+//  public void start(Stage primaryStage) {
+//    MVC mvc = new MVC();
+//    mvc.start(primaryStage);
+//  }
 
 }
--- src/droplauncher/mvc/MVC.java
+++ src/droplauncher/mvc/MVC.java
@@ -2,28 +2,26 @@ package droplauncher.mvc;
 
 import droplauncher.mvc.controller.Controller;
 import droplauncher.mvc.model.Model;
-import droplauncher.mvc.view.View;
-import javafx.stage.Stage;
 
 public class MVC {
 
   private Controller controller;
   private Model model;
-  private View view;
+//  private View view;
 
   public MVC() {
     this.controller = new Controller();
     this.model = new Model();
-    this.view = new View();
+//    this.view = new View();
 
     this.controller.setModel(this.model);
-    this.controller.setView(this.view);
+//    this.controller.setView(this.view);
 
-    this.view.setController(this.controller);
+//    this.view.setController(this.controller);
   }
 
-  public void start(Stage primaryStage) {
-    this.view.start(primaryStage);
-  }
+//  public void start(Stage primaryStage) {
+//    this.view.start(primaryStage);
+//  }
 
 }
