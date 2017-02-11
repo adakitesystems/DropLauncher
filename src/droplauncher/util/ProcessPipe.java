58c17867869ef52d241eb57c5fcfb5c4e29cfb12
Adakite Systems <adakitesystems@gmail.com>
2017-02-11 11:56:45 -0600

    Rework ProcessPipe.java

--- src/droplauncher/util/ProcessPipe.java
+++ src/droplauncher/util/ProcessPipe.java
@@ -2,48 +2,39 @@ package droplauncher.util;
 
 import adakite.debugging.Debugging;
 import adakite.util.AdakiteUtils;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
-import java.util.Arrays;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Generic class for handling communication between the main program and
  * a process.
  */
-public class ProcessPipe extends Thread {
+public class ProcessPipe {
 
-  public static final double DEFAULT_READ_TIMEOUT = (double)0.25; /* seconds */
+  private static final Logger LOGGER = LogManager.getLogger();
 
-  private Path path;
-  private String[] args;
   private Process process;
-  private InputStream is;
-  private BufferedReader br; /* read from process */
-  private OutputStream os;
-  private BufferedWriter bw; /* write to process */
+  private String[] args;
+  private Path cwd;
   private StreamGobbler gobblerStdout;
   private StreamGobbler gobblerStderr;
-  private Path cwd;
 
   public ProcessPipe() {
-    this.path = null;
-    this.args = null;
     this.process = null;
-    this.is = null;
-    this.br = null;
-    this.os = null;
-    this.bw = null;
+    this.args = null;
+    this.cwd = null;
     this.gobblerStdout = null;
     this.gobblerStderr = null;
-    this.cwd = null;
+  }
+
+  public String[] getArgs() {
+    return this.args;
+  }
+
+  public void setArgs(String[] args) {
+    this.args = args;
   }
 
   public Path getCWD() {
@@ -54,68 +45,34 @@ public class ProcessPipe extends Thread {
     this.cwd = path;
   }
 
-  /**
-   * Open a pipe to the specified program.
-   *
-   * @param path specified program
-   * @param args arguments to include during invocation
-   * @throws UnsupportedEncodingException
-   * @throws IOException
-   * @throws IllegalArgumentException if Path argument is null
-   */
-  public void open(Path path, String[] args)
-      throws UnsupportedEncodingException,
-             IOException {
-    if (path == null) {
-      throw new IllegalArgumentException(Debugging.nullObject("path"));
-    }
-
-    String[] command;
+  public void open(String[] args) throws IOException {
     if (args == null) {
-      command = new String[1];
-    } else {
-      this.args = Arrays.copyOf(args, args.length);
-      command = new String[this.args.length + 1];
-      System.arraycopy(this.args, 0, command, 1, this.args.length);
+      throw new IllegalArgumentException(Debugging.nullObject("args"));
     }
 
-    this.path = path;
-    command[0] = this.path.toString();
-    if (AdakiteUtils.directoryExists(this.cwd)) {
+    this.args = args;
+
+    ProcessBuilder pb = new ProcessBuilder(this.args);
+    if (this.cwd != null && AdakiteUtils.directoryExists(this.cwd)) {
       /* Set current working directory for the new process. */
-      ProcessBuilder pb = new ProcessBuilder(command);
       pb.directory(this.cwd.toFile());
-      this.process = pb.start();
-    } else {
-      this.process = new ProcessBuilder(command).start();
     }
 
-    this.is = this.process.getInputStream();
-    this.br = new BufferedReader(new InputStreamReader(this.is, StandardCharsets.UTF_8));
+    this.process = pb.start();
+
     this.gobblerStdout = new StreamGobbler(this.process.getInputStream());
     this.gobblerStderr = new StreamGobbler(this.process.getErrorStream());
     this.gobblerStdout.start();
     this.gobblerStderr.start();
-
-    this.os = this.process.getOutputStream();
-    this.bw = new BufferedWriter(new OutputStreamWriter(this.os, StandardCharsets.UTF_8));
   }
 
-  /**
-   * Closes the pipe and destroys the process.
-   */
-  public void close() throws IOException {
-    this.gobblerStderr.interrupt();
+  public void close() {
     this.gobblerStdout.interrupt();
-    this.br.close();
-    this.is.close();
-    this.bw.close();
-    this.os.close();
-  }
-
-  @Override
-  public void run() {
-
+    this.gobblerStdout.interrupt();
+    this.process.destroy();
+    if (this.process.isAlive()) {
+      LOGGER.warn("process is still alive after attempting to destroy");
+    }
   }
 
 }
