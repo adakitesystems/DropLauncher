99fd67440c11a7fd209805e695cd9826a1314221
Adakite Systems <adakitesystems@gmail.com>
2017-02-13 10:25:42 -0600

    Add file DirectoryMonitor.java

--- src/droplauncher/util/DirectoryMonitor.java
+++ src/droplauncher/util/DirectoryMonitor.java
@@ -3,45 +3,87 @@ package droplauncher.util;
 import adakite.util.AdakiteUtils;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+/**
+ * Class for monitoring a directory structure.
+ */
 public class DirectoryMonitor {
 
   private Path path;
-  private Path[] prevFiles;
-  private Path[] currFiles;
-  private Path[] newFiles;
+  private List<Path> prevFiles;
+  private List<Path> currFiles;
+  private List<Path> newFiles;
 
   private DirectoryMonitor() {}
 
   public DirectoryMonitor(Path directory) {
     this.path = directory;
-    this.prevFiles = new Path[]{};
-    this.currFiles = new Path[]{};
-    this.newFiles = new Path[]{};
+    this.prevFiles = new ArrayList<>();
+    this.currFiles = new ArrayList<>();
+    this.newFiles = new ArrayList<>();
   }
 
+  /**
+   * Returns the path to the currently monitored directory.
+   */
   public Path getPath() {
     return this.path;
   }
 
-  public Path[] getPreviousFiles() {
+  /**
+   * Returns the contents of the directory observed at the last {@link #reset()}.
+   */
+  public List<Path> getPreviousFiles() {
     return this.prevFiles;
   }
 
-  public Path[] getCurrentFiles() {
+  /**
+   * Returns the contents of the directory observed at the last
+   * {@link #reset()} or {@link #update()}.
+   */
+  public List<Path> getCurrentFiles() {
     return this.currFiles;
   }
 
-  public Path[] getNewFiles() {
+  /**
+   * Returns the contents of the directory observed at the last
+   * {@link #reset()} or {@link #update()}.
+   */
+  public List<Path> getNewFiles() {
     return this.newFiles;
   }
 
+  /**
+   * Resets the new files list and refreshes both previous and current files
+   * lists.
+   *
+   * @throws IOException if an I/O error occurs
+   */
   public void reset() throws IOException {
-    this.prevFiles = AdakiteUtils.getDirectoryContents(this.path);
-    this.currFiles = AdakiteUtils.getDirectoryContents(this.path);
-    this.newFiles = AdakiteUtils.getDirectoryContents(this.path);
+    this.newFiles.clear();
+    this.prevFiles = Arrays.asList(AdakiteUtils.getDirectoryContents(this.path));
+    this.currFiles = new ArrayList<>(this.prevFiles);
   }
 
-
+  /**
+   * Refreshes the current files list and fills the new files list by comparing
+   * the previous files list to the current files list.
+   *
+   * @see #getNewFiles()
+   * @throws IOException if an I/O error occurs
+   */
+  public void update() throws IOException {
+    this.newFiles.clear();
+    this.currFiles = Arrays.asList(AdakiteUtils.getDirectoryContents(this.path));
+    for (Path currFile : this.currFiles) {
+      if (!this.prevFiles.contains(currFile)) {
+        this.newFiles.add(currFile);
+      }
+    }
+    this.prevFiles = new ArrayList<>(this.currFiles);
+  }
 
 }
