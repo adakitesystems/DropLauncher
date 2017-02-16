ea3a33a1ad8f55be48e17c9eb6c4f4a2c85c6a67
Adakite Systems <adakitesystems@gmail.com>
2017-02-16 13:24:03 -0600

    Add method "isNonNullAndEqual" to "AdakiteUtils.java"

--- src/adakite/util/AdakiteUtils.java
+++ src/adakite/util/AdakiteUtils.java
@@ -56,6 +56,17 @@ public class AdakiteUtils {
   }
 
   /**
+   * Tests whether the specfied two strings are both non-null and equal
+   * to each other.
+   *
+   * @param str1 specified first string
+   * @param str2 specified second string
+   */
+  public static boolean isNonNullAndEqual(String str1, String str2) {
+    return (str1 != null && str2 != null && str1.equals(str2));
+  }
+
+  /**
    * Returns the integer value of the specified string.
    *
    * @param str the specified string to convert to an integer
