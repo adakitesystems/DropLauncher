428c80996655279909e9f74ab8ac9b56298c3979
Adakite Systems <adakitesystems@gmail.com>
2017-04-28 16:26:55 -0600

    Move enum "NetworkProvider" to "BWHeadless.java"

--- src/droplauncher/bwheadless/BWHeadless.java
+++ src/droplauncher/bwheadless/BWHeadless.java
@@ -114,6 +114,47 @@ public class BWHeadless {
 
   }
 
+  /**
+   * Enum for the passable network argument to the bwheadless process.
+   * Currently, only LAN is supported since LocalPC requires admin privileges
+   * and a modified SNP file.
+   */
+  public enum NetworkProvider {
+
+    LAN("lan"),
+    ;
+
+    private final String str;
+
+    private NetworkProvider(String str) {
+      this.str = str;
+    }
+
+    /**
+     * Returns the corresponding NetworkProvider object.
+     *
+     * @param str specified string
+     * @return
+     *     the corresponding NetworkProvider object,
+     *     otherwise null if no match was found
+     */
+    public static NetworkProvider get(String str) {
+      str = str.toLowerCase(Locale.US);
+      for (NetworkProvider val : NetworkProvider.values()) {
+        if (str.equals(val.toString().toLowerCase(Locale.US))) {
+          return val;
+        }
+      }
+      return null;
+    }
+
+    @Override
+    public String toString() {
+      return this.str;
+    }
+
+  }
+
   public static final Path DEFAULT_EXE_PATH = Paths.get("bwheadless.exe");
   public static final String DEFAULT_INI_SECTION_NAME = "bwheadless";
 
