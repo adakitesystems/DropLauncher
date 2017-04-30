f0b8ecf133738b47fd2fde341286098ed4db1302
Adakite Systems <adakitesystems@gmail.com>
2017-04-29 21:37:20 -0600

    Add enum "BWAPI.ExtractableFile"

--- src/droplauncher/bwapi/BWAPI.java
+++ src/droplauncher/bwapi/BWAPI.java
@@ -91,11 +91,45 @@ public class BWAPI {
 
   }
 
+  /**
+   * Enum for extractable files.
+   *
+   * sha256 checksums:
+   * 60e69e90943073696458cd2e5ace9baa39c542cb4ec1ab1d2adfe5dba18035ee *Broodwar.map
+   * d16c2909993b9de89b8a285c89cf17e8b16375c876d3ebfd443fde31c9d504a5 *bwapi.ini
+   * 28282d77de250e4c7dd7b5ac1563c62d43465a2dda19119f551b45e7dc0d2aa7 *gmp-vc90-mt-gd.dll
+   * 9f45860228df80656d2c7407a0fa6d82c1b759d47c7dee843877700740eb4b58 *gmp-vc90-mt.dll
+   * b1873ca36d8ff3f0df0bbf1895916cc13e4bb95588e0e711c689bd37ead8100f *mpfr-vc90-mt-gd.dll
+   * 647760f4b63ce1a4c36de4c71176f59cfccdfbb9ad397979725228272c8c67ae *mpfr-vc90-mt.dll
+   */
+  public enum ExtractableFile {
+
+    BROODWAR_MAP("Broodwar.map"),  /* Exception Filter input file for BWAPI */
+    BWAPI_INI("bwapi.ini"),        /* BWAPI configuration file */
+    GMP("gmp-vc90-mt.dll"),        /* BWTA dependency */
+    GMP_GD("gmp-vc90-mt-gd.dll"),  /* BWTA dependency */
+    MPFR("mpfr-vc90-mt.dll"),      /* BWTA dependency */
+    MPFR_GD("mpfr-vc90-mt-gd.dll") /* BWTA dependency */
+    ;
+
+    private final String str;
+
+    private ExtractableFile(String str) {
+      this.str = str;
+    }
+
+    public String toString() {
+      return this.str;
+    }
+
+  }
+
   public static final Path DATA_PATH = Paths.get("bwapi-data");
   public static final Path DATA_AI_PATH = DATA_PATH.resolve(Paths.get("AI"));
   public static final Path DATA_INI_PATH = DATA_PATH.resolve(Paths.get("bwapi.ini"));
   public static final Path DATA_READ_PATH = DATA_PATH.resolve(Paths.get("read"));
   public static final Path DATA_WRITE_PATH = DATA_PATH.resolve(Paths.get("write"));
+  public static final Path DATA_DATA_PATH = DATA_PATH.resolve(Paths.get("data")); /* mostly for Broodwar.map */
   public static final String DLL_UNKNOWN = "Unknown";
 
   public static final String DEFAULT_INI_SECTION_NAME = "bwapi";
